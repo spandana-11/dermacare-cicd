e9280b3b0b8da1326f45c4db6f88eea2de5c7cae
Sai Manikanta <ch.saimanikanta92@gmai.com>
2025-10-24 15:48:50 +0530

    update

---
+++ customerservice/src/main/java/com/dermaCare/customerService/dto/TreatmentResponseDTO.java
@@ -0,0 +1,23 @@
+package com.dermaCare.customerService.dto;
+
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TreatmentResponseDTO {
+    private List<String> selectedTestTreatment;
+    private Map<String, TreatmentDetailsDTO> generatedData; // dynamic treatment names
+
+    // ✅ Sitting summary fields (optional)
+    private int totalSittings;
+    private int pendingSittings;
+    private int takenSittings;
+    private int currentSitting;
+}
