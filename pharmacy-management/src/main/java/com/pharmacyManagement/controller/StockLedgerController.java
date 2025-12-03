ff027a7500f9d6c1bdc3415591c9ef77d16c74d8
Sai Manikanta <ch.saimanikanta92@gmai.com>
2025-12-03 17:21:06 +0530

    updated

--- pharmacy-management/src/main/java/com/pharmacyManagement/controller/StockLedgerController.java
+++ pharmacy-management/src/main/java/com/pharmacyManagement/controller/StockLedgerController.java
@@ -9,30 +9,42 @@ import com.pharmacyManagement.service.StockLedgerService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/stock")
+@RequestMapping("/stock")
 @RequiredArgsConstructor
 public class StockLedgerController {
 
     private final StockLedgerService stockService;
 
-    @PostMapping("/purchase")
-    public Response addPurchase(@RequestBody StockDTO dto) {
-        return stockService.addPurchaseStock(dto);
+    // -----------------------
+    // PURCHASE STOCK
+    // -----------------------
+    @PostMapping("/purchase/{purchaseBillNo}")
+    public Response addPurchase(@PathVariable String purchaseBillNo,
+                                @RequestBody StockDTO dto) {
+        return stockService.addPurchaseStock(dto, purchaseBillNo);
     }
 
-    @PostMapping("/sale")
-    public Response addSale(@RequestParam String productId,
-                            @RequestParam String batchNo,
-                            @RequestParam int qty,
-                            @RequestParam String saleId) {
+    // -----------------------
+    // SALE STOCK
+    // -----------------------
+    @PostMapping("/sale/{productId}/{batchNo}/{qty}/{saleId}")
+    public Response addSale(@PathVariable String productId,
+                            @PathVariable String batchNo,
+                            @PathVariable int qty,
+                            @PathVariable String saleId) {
+
         return stockService.recordSale(productId, batchNo, qty, saleId);
     }
 
-    @PostMapping("/damage")
-    public Response addDamage(@RequestParam String productId,
-                              @RequestParam String batchNo,
-                              @RequestParam int qty,
-                              @RequestParam String reason) {
+    // -----------------------
+    // DAMAGE STOCK
+    // -----------------------
+    @PostMapping("/damage/{productId}/{batchNo}/{qty}/{reason}")
+    public Response addDamage(@PathVariable String productId,
+                              @PathVariable String batchNo,
+                              @PathVariable int qty,
+                              @PathVariable String reason) {
+
         return stockService.recordDamage(productId, batchNo, qty, reason);
     }
 }
