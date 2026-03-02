d6d7ea029d66dd3bb3ffb038b06ba5d52ea869e4
Sai Manikanta <ch.saimanikanta92@gmai.com>
2026-03-02 14:05:42 +0530

    updated

--- pharmacy-management/src/main/java/com/pharmacyManagement/repository/MedicineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pharmacyManagement.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.pharmacyManagement.entity.Medicine;
-
-public interface MedicineRepository extends MongoRepository<Medicine, String> {
-
-    boolean existsByMedicineNameIgnoreCase(String medicineName);
-}
