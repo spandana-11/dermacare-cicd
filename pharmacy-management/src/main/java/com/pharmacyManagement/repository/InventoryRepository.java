143f8029c7743aed3565ecc9c92593663b170cb3
Sai Manikanta <ch.saimanikanta92@gmai.com>
2026-03-13 13:07:43 +0530

    updated

--- pharmacy-management/src/main/java/com/pharmacyManagement/repository/InventoryRepository.java
+++ pharmacy-management/src/main/java/com/pharmacyManagement/repository/InventoryRepository.java
@@ -8,14 +8,8 @@ import com.pharmacyManagement.entity.Inventory;
 
 public interface InventoryRepository extends MongoRepository<Inventory, String> {
 
-	void deleteByProductIdAndBatchNo(String productId, String batchNo);
-
-	Inventory findByProductIdAndBatchNo(String productId, String batchNo);
-	
-
 	Inventory findByMedicineIdAndBatchNoAndClinicIdAndBranchId(String medicineId, String batchNo, String clinicId,
 			String branchId);
-//	Inventory findByProductIdAndBatchNo(String productId, String batchNo);
     List<Inventory> findByClinicIdAndBranchId(String clinicId, String branchId);
 	Inventory findByMedicineIdAndBatchNo(String medicineId, String batchNo);
 	 Inventory findByMedicineIdAndBatchNoAndExpiryDateAndClinicIdAndBranchId(
